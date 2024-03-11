ff3f19afce5a2605efac0916fb3e1b5b3f8408f2
namanraj77 <namanraj7033@gmail.com>
2024-03-11 09:08:14 +0530

    first commit

---
+++ Main/script.js
@@ -0,0 +1,11 @@
+var typed= new Typed("#auto-type",{
+  strings :["Web Developer","Software Developer","Content Creator"],
+  typeSpeed: 35,
+  backSpeed: 35,
+  loop: true
+})
+/*const scroll1 = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});*/
+AOS.init();// for scroll reveal animation
