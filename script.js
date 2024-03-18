a1b10395ea56fbd0ffef34f3e915eb24fcdde3a8
namanraj77 <namanraj7033@gmail.com>
2024-03-18 18:58:42 +0530

    Custom cursor

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ var typed= new Typed("#auto-type",{
   smooth: true
 });*/
 AOS.init();// for scroll reveal animation
+var main= document.querySelector("#main")
+var crsr= document.querySelector("#cursor")
+main.addEventListener("mousemove", function(dets){
+  crsr.style.left=dets.x+"px"// dets mouse movement ko track karta hai, in built function hai
+  crsr.style.top=dets.y+"px"
+})
